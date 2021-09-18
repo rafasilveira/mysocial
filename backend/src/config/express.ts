49457d1bb3b6f820182bfc0932fdd0eef13a1cc4
rafasilveira <oi@rafasilveira.dev>
2021-09-18 16:57:07 -0300

    feat(backend): logger middleware

--- backend/src/config/express.ts
+++ backend/src/config/express.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import { loggerMiddleware } from '../middleware/logger.middleware'
 
 const app = express()
 app.use(express.json())
+app.use(loggerMiddleware)
 
 export default app
