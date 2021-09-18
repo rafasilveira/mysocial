699f82dabb9cc8d13c9427d5727788fab2827fca
rafasilveira <oi@rafasilveira.dev>
2021-09-18 16:42:18 -0300

    feat(backend): improve search with regex

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -3,9 +3,28 @@ import db from '../data/db.json'
 
 export const usersController = (filter: UsersFilter = {}): Promise<User[]> =>
   new Promise((resolve) => {
-    const data = filter?.name
-      ? db.filter((item) => item.name == filter?.name)
-      : db
+    let data = db
+
+    if (filter?.name) {
+      const name = filter?.name ?? ''
+
+      // in a real case scenario, this would be externalized in a helper function
+      const re = new RegExp(
+        name
+          .replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&')
+          .replace(/[a,á,à,ä,ã,â]/gi, 'a')
+          .replace(/[e,é,ë,ê]/gi, 'e')
+          .replace(/[i,í,ï]/gi, 'i')
+          .replace(/[o,ó,ö,ò,õ,ô]/gi, 'o')
+          .replace(/[u,ü,ú,ù]/gi, 'u')
+          .replace(/[c,ç]/gi, 'c')
+          .toLowerCase()
+          .split(' ')
+          .map((p) => `[a-z]*${p}[a-z]*`)
+          .join(' ')
+      )
+      data = db.filter((item) => item.name.toLowerCase().match(re))
+    }
 
     // simulating a delay from a db call
     setTimeout(() => {
